7d6377738d1697e063b045e60d6c87e2f924edd6
Theglxry <102861719+Theglxry@users.noreply.github.com>
2023-11-15 19:52:23 +0100

    update

--- src/Components/stats.jsx
+++ src/Components/stats.jsx
@@ -1,4 +1,3 @@
-
 function Stats({ ipAddress, location, timeZone, isp }) {
   // Check if any of the data is undefined, and if so, return null to avoid rendering
   if (!ipAddress || !location || !timeZone || !isp) {
@@ -6,9 +5,9 @@ function Stats({ ipAddress, location, timeZone, isp }) {
   }
 
   return (
-    <div className="shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] top-[16rem]
-     md:mt-4 w-4/5 rounded-lg absolute  md:top-60 left-[5rem] md:left-40
-     bg-white z-50 flex flex-col sm:flex-row container gap-4 md:gap-10 p-4 md:p-8 ">
+    <div
+      className="shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)] top-[16rem] md:mt-4 w-4/5 rounded-lg absolute  md:top-60 left-[5rem] md:left-40 bg-white z-50 flex flex-col sm:flex-row container gap-4 md:gap-10 p-4 md:p-8 "
+    >
       <div className="md:border-r-2 w-full border-slate-300 flex flex-col gap-2  items-center">
         <p className="text-sm text-slate-400 font-semibold">IP ADDRESS</p>
         <p className="md:text-2xl text-black font-bold">{ipAddress}</p>
@@ -33,4 +32,3 @@ function Stats({ ipAddress, location, timeZone, isp }) {
 }
 
 export default Stats;
-
